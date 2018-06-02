7ed95bf1802cdab01c93c7fac679730dd3994963
Rifan Ardiansyah <rifanardiansyah1@gmail.com>
2018-06-02 22:51:20 +0700

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "pics#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pics
   get "pics/:id/download", to: 'pics#download'
